6b1bd114fe3745005f8d189dfa18e69c5463ef98
erulli <117382591+onion-milkshake@users.noreply.github.com>
2023-08-01 22:44:48 +0530

    Added DOM logic

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
-'use strict'; 
+'use strict';
 
-console.log(document.querySelector('.guess-result')
-.textContent);
-// document.querySelector('.guess-result').textContent = 'Correct Number !!'
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
-// document.querySelector('.guess').value = 22;
-// let element = document.querySelector('.guess').value;
-// console.log(element)
+// console.log(document.getElementById('button-check').textContent);
 
 document.querySelector('.check').addEventListener(
-    'click', function() {
-        console.log(document.querySelector('.guess').value)
+    'click', function () {
+        const guessedNumber = Number(document.querySelector('.guess').value);
+        // console.log(typeof guessedNumber);
+
+        if (!guessedNumber) { // if guessedNumber not 0
+            document.querySelector('.guess-result').textContent = '⛔ No Number!'
+        }
+        else if (guessedNumber === secretNumber) {
+            document.querySelector('.guess-result').textContent = '🎉 Guess Correct!'
+        }
+        else {
+
+            score--;
+            document.querySelector('.span-score').textContent = score;
+
+            if (score > 0) {
+                if (Math.abs(guessedNumber - secretNumber) == 1 ||
+                    Math.abs(guessedNumber - secretNumber) == 2) {
+                    document.querySelector('.guess-result').textContent = '🤞 Very Close!';
+                }
+                else {
+                    if (guessedNumber > secretNumber) {
+                        document.querySelector('.guess-result').textContent = '📈 Too High!';
+                    }
+                    else {
+                        document.querySelector('.guess-result').textContent = '📉 Too Low!';
+                    }
+                }
+            }
+            else {
+                document.querySelector('.guess-result').textContent = '💥 You Lost!'
+                document.getElementById('button-check').disabled = true;
+            }
+        }
     }
 )
