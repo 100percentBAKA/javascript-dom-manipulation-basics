8ddbe51a06d95388859a0357b2d2d51b4ce85e83
erulli <117382591+onion-milkshake@users.noreply.github.com>
2023-08-03 09:58:49 +0530

    Added DOM logic
    Added DOM logic to handle clicks and Escape key event

--- modal-window/script.js
+++ modal-window/script.js
@@ -1,3 +1,39 @@
 'use strict';
 
-var hello = "hello";
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const buttonCloseModal = document.querySelector('.close-modal-button');
+const buttonOpenModal = document.querySelectorAll('.open-modal-button');
+
+console.log(buttonOpenModal);
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+// buttonOpenModal is a node list
+for (let i = 0; i < buttonOpenModal.length; i++) {
+    buttonOpenModal[i].addEventListener(
+        'click', openModal
+    )
+}
+
+buttonCloseModal.addEventListener(
+    'click', closeModal
+)
+
+// Handling key events
+document.addEventListener(
+    'keydown', function (event) {
+        if (event.key == 'Escape' && !modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    }
+)
