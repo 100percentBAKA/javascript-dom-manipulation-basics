634df5be67ca78b5f4f1f01b24d545fb424661a0
erulli <117382591+onion-milkshake@users.noreply.github.com>
2023-08-24 12:09:13 +0530

    Revert "Modify script"
    This reverts commit bfde4e5283b64748f8ed0e06619731f2479b572d.

--- interactive-bubble/script.js
+++ interactive-bubble/script.js
@@ -1,17 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
     const wrapper = document.querySelector('#bubble-wrapper');
 
-    const animateBubble = x => {
+    const getRandomColor = () => {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+    const animateBubble = (x, y) => {
         const bubble = document.createElement("div");
 
         bubble.className = "bubble";
+        bubble.style.opacity = 0;
+        bubble.style.backgroundColor = getRandomColor();
 
         bubble.style.left = `${x}px`;
+        bubble.style.top = `${y}px`;
 
         wrapper.appendChild(bubble);
 
-        setTimeout(() => wrapper.removeChild(bubble), 2000);
+        bubble.style.transition = "opacity 0.5s";
+        bubble.style.opacity = 1;
+
+        setTimeout(() => {
+
+            bubble.style.opacity = 0;
+            setTimeout(() => {
+                wrapper.removeChild(bubble);
+            }, 500);
+        }, 2000);
     }
 
-    window.onmousemove = event => animateBubble(event.clientX);
+    window.onmousemove = event => {
+        animateBubble(event.clientX, event.clientY);
+    };
 });
