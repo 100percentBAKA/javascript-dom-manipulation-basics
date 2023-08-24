08ead860559e268f0106493ccc853d26b1659d04
erulli <117382591+onion-milkshake@users.noreply.github.com>
2023-08-24 12:11:02 +0530

    Initial Commit

---
+++ interactive-bubble-2/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const wrapper = document.querySelector('#bubble-wrapper');
+
+    const animateBubble = x => {
+        const bubble = document.createElement("div");
+
+        bubble.className = "bubble";
+
+        bubble.style.left = `${x}px`;
+
+        wrapper.appendChild(bubble);
+
+        setTimeout(() => wrapper.removeChild(bubble), 2000);
+    }
+
+    window.onmousemove = event => animateBubble(event.clientX);
+});
