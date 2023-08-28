82ae2f8ce11a847130569a13928d4bd06c3f5910
erulli <117382591+onion-milkshake@users.noreply.github.com>
2023-08-28 20:45:35 +0530

    Initial commit

---
+++ scroll-animations/script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenEle = document.querySelectorAll('.hidden');
+hiddenEle.forEach((element) => {
+    observer.observe(element);
+});
